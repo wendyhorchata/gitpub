b7919fdded11c1b97e3b0ab9ffcb2e578342da51
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-11 19:09:53 -0700

    third commit

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 // Setting up the express
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = 8080;
+const drinks = require('./models/drinks.js')
+const foods = require('./models/foods.js')
 
-
-const drinks = require('./models/drinks')
-const foods = require('./models/foods')
-
-app.use(function(req, res, next) {
-    res.setHeader("Content-Type", "image/png");
-    next();
-});
 
 // route all Action
 app.get('/', (req, res) => {
@@ -24,26 +18,25 @@ app.get('/drinks', (req, res) => {
     })
 })
 
+app.get('/drinks/:id', (req, res) => {
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id]
+    })
+})
+
+
 app.get('/foods', (req, res) => {
     res.render('foods_index.ejs', {
-        allDrinks: drinks,
         allFoods: foods
     })
 })
 
-// route show one item
-app.get('/drinks/:id', (req, res) => {
-        // res.type('png')
-        res.render('drinks_show.ejs', {
-            drink: drinks[req.params.id]
-        })
-})
+
 
 
 app.get('/foods/:id', (req, res) => {
-    //   res.type('png')
       res.render('foods_show.ejs', {
-        food: foods[req.params.id]
+        item: foods[req.params.id]
         
     })
        
@@ -51,7 +44,7 @@ app.get('/foods/:id', (req, res) => {
 
 
 // listen to port
-app.listen(port, () => {
-    console.log('Listening to port: ', port)
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`)
 })
 
